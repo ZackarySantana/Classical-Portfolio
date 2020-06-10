19d16a06ebeaaf1007280a8d0a08212c0aea28b3
ZackarySantana <ZackZackYack@gmail.com>
2020-06-10 16:00:21 -0400

    Removed styled component and changed general styles for sidenav

--- src/resume/sidenav/index.js
+++ src/resume/sidenav/index.js
@@ -9,9 +9,6 @@ import './styles.scss';
 import * as Links from '../components/links';
 import { Github, LinkedIn } from '../components/icons';
 
-const List = styled.ul`
-`;
-
 export default function SideNav() {
 	const [active, setActive] = useState(0);
 
@@ -30,15 +27,17 @@ export default function SideNav() {
 	}, []);
 
 	return (
-		<List id="sidenav">
-			<li><p className={"link" + (active === 0 ? " active" : "")} onClick={Links.scrollToHome}>Home</p></li>
-			<li><p className={"link" + (active === 1 ? " active" : "")} onClick={Links.scrollToResume}>Resume</p></li>
-			<li><p className={"link" + (active === 2 ? " active" : "")} onClick={Links.scrollToProjects}>Projects</p></li>
-			<li><p className={"link" + (active === 3 ? " active" : "")} onClick={Links.scrollToContactMe}>Contact Me</p></li>
-			<li id="icons">
-				<Github onClick={Links.openGithub} />
-				<LinkedIn onClick={Links.openLinkedIn} />
-			</li>
-		</List >
+		<div id="sidenav" className={active > 0 ? "expand" : ""}>
+			<ul>
+				<li><p className={"link" + (active === 0 ? " active" : "")} onClick={Links.scrollToHome}>Home</p></li>
+				<li><p className={"link" + (active === 1 ? " active" : "")} onClick={Links.scrollToResume}>Resume</p></li>
+				<li><p className={"link" + (active === 2 ? " active" : "")} onClick={Links.scrollToProjects}>Projects</p></li>
+				<li><p className={"link" + (active === 3 ? " active" : "")} onClick={Links.scrollToContactMe}>Contact Me</p></li>
+				<li id="icons">
+					<Github onClick={Links.openGithub} />
+					<LinkedIn onClick={Links.openLinkedIn} />
+				</li>
+			</ul>
+		</div>
 	);
 }
