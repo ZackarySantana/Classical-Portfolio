0ea9be5791519ecbecb2eba9107f72866ef69ba9
ZackarySantana <ZackZackYack@gmail.com>
2020-06-23 10:42:58 -0400

    Changed store and updated references

--- src/store.js
+++ src/store.js
@@ -1,29 +1,9 @@
 // Libraries
 import { createStore } from 'redux';
 
-
-const store = createStore(function () { }, loadFromLocalState());
-
-function loadFromLocalState() {
-	try {
-		const serializedState = localStorage.getItem('store');
-		if (serializedState === null) return undefined;
-		return JSON.parse(serializedState);
-	} catch (exception) {
-		console.log(exception);
-		return undefined;
-	}
-}
-
-function saveToLocalStorage(state) {
-	try {
-		const serializedState = JSON.stringify(state);
-		localStorage.setItem('store', serializedState);
-	} catch (exception) {
-		console.log(exception);
-	}
-}
-
-store.subscribe(() => saveToLocalStorage(store.getState()));
+const store = createStore(
+	function () { },
+	{},
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
